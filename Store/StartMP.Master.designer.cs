2751491dd4f5f56d97f05c542174dcf359f89b57
Svyatoslav <beuker93@gmail.com>
2015-03-16 15:47:40 +0200

    branches test

--- Store/StartMP.Master.designer.cs
+++ Store/StartMP.Master.designer.cs
@@ -49,6 +49,15 @@ namespace Store {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+        
+        /// <summary>
         /// menuBar1 control.
         /// </summary>
         /// <remarks>
